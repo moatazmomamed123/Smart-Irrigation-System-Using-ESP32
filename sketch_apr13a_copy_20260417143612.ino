225985a96c5736b9c696257b97f042719efd1b15
moatazmomamed123 <moatazmohamedragep@gmail.com>
2026-04-19 23:58:49 +0200

    Update sketch_apr13a_copy_20260417143612.ino

--- sketch_apr13a_copy_20260417143612.ino
+++ sketch_apr13a_copy_20260417143612.ino
@@ -2,15 +2,12 @@
 #include <Firebase_ESP_Client.h>
 #include <DHT.h>
 
-// 1. إعدادات الواي فاي
 #define WIFI_SSID "test"
 #define WIFI_PASSWORD "123456789"
 
-// 2. إعدادات Firebase (تم التصحيح بناءً على الصورة)
 #define DATABASE_URL "https://smartplant-9df1d-default-rtdb.europe-west1.firebasedatabase.app"
 #define DATABASE_SECRET "cITEBGl5Xy1gxZJpfDgYrJzR8ds52j1bdtNhlt5z"
 
-// 3. تعريف الدبابيس
 #define DHTPIN 4
 #define DHTTYPE DHT11
 #define SOIL_PIN 34
@@ -50,40 +47,31 @@ void setup() {
 void loop() {
   if (WiFi.status() != WL_CONNECTED) return;
 
-  // قراءة الحساسات
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   int soilValue = analogRead(SOIL_PIN);
   int ldrValue = analogRead(LDR_PIN);
   int soilPercent = map(soilValue, 4095, 1500, 0, 100);
   soilPercent = constrain(soilPercent, 0, 100);
-  int lightPercent = map(ldrValue, 0, 4095, 100, 0); // تحويل الإضاءة لنسبة
-
-  // 1. جلب الإعدادات من Firebase
-  // جلب وضع الري التلقائي (auto_irrigation)
+  int lightPercent = map(ldrValue, 0, 4095, 100, 0);
   if (Firebase.RTDB.getBool(&fbdo, "/controls/auto_irrigation")) {
     autoMode = fbdo.boolData();
   }
 
-  // 2. منطق التحكم في المضخة
   if (autoMode) {
-    // وضع تلقائي: تعتمد على الحساس (أقل من 30%)
     if (soilPercent < 30) digitalWrite(PUMP_PIN, HIGH);
     else digitalWrite(PUMP_PIN, LOW);
   } else {
-    // وضع يدوي: تعتمد على زر الـ Pump في Firebase
     if (Firebase.RTDB.getBool(&fbdo, "/controls/pump")) {
       if (fbdo.boolData()) digitalWrite(PUMP_PIN, HIGH);
       else digitalWrite(PUMP_PIN, LOW);
     }
   }
 
-  // 3. رفع البيانات إلى Firebase كل 5 ثوانٍ
   if (millis() - sendDataPrevMillis > 5000) {
     sendDataPrevMillis = millis();
 
     FirebaseJson updateData;
-    // تحديث البيانات في مجلد sensor_data ليتوافق مع تطبيق الفلتر
     updateData.set("sensor_data/temp", t);
     updateData.set("sensor_data/moisture", soilPercent);
     updateData.set("sensor_data/light", lightPercent);
